bb3075a0dcee377da70076b79ed6ed1524ede0a4
hhllhhyyds <hhllhhyyds@sina.com>
2024-03-24 13:57:36 +0800

    feat(examples): vary color according to normal

--- examples/e4_teapot/main.rs
+++ examples/e4_teapot/main.rs
@@ -20,14 +20,17 @@ fn main() {
     .unwrap();
 
     let vertex_shader_src = r#"
-        #version 140
+        #version 150      
 
         in vec3 position;
         in vec3 normal;
-
+        
+        out vec3 v_normal;      
+        
         uniform mat4 matrix;
-
+        
         void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;      
             gl_Position = matrix * vec4(position, 1.0);
         }
     "#;
@@ -35,10 +38,15 @@ fn main() {
     let fragment_shader_src = r#"
         #version 140
 
+        in vec3 v_normal;
         out vec4 color;
-
+        uniform vec3 u_light;
+        
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
         }
     "#;
 
@@ -64,13 +72,15 @@ fn main() {
                             [0.0, 0.0, 0.01, 0.0],
                             [0.0, 0.0, 0.0, 1.0f32],
                         ];
+                        // the direction of the light
+                        let light = [-1.0, 0.4, 0.9f32];
 
                         target
                             .draw(
                                 (&positions, &normals),
                                 &indices,
                                 &program,
-                                &uniform! { matrix: matrix },
+                                &uniform! { matrix: matrix, u_light: light },
                                 &Default::default(),
                             )
                             .unwrap();
