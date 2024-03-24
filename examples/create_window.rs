cabb7f739801b99a847881a0be342ef1f2c73964
hhllhhyyds <hhllhhyyds@sina.com>
2024-03-24 10:01:51 +0800

    Fill framebuffer with color

--- examples/create_window.rs
+++ examples/create_window.rs
@@ -1,8 +1,14 @@
+use glium::Surface;
+
 fn main() {
     let event_loop = winit::event_loop::EventLoopBuilder::new()
         .build()
         .expect("event loop building");
-    let (_window, _display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
+    let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
+
+    let mut frame = display.draw();
+    frame.clear_color(0.0, 0.0, 1.0, 1.0);
+    frame.finish().unwrap();
 
     let _ = event_loop.run(move |event, window_target| {
         match event {
