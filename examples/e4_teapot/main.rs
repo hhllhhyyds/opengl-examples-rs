a7052c866397fc63b5b457f67ca8907a4ece0c68
hhllhhyyds <hhllhhyyds@sina.com>
2024-03-24 15:57:11 +0800

    feat(examples): add face cull

--- examples/e4_teapot/main.rs
+++ examples/e4_teapot/main.rs
@@ -45,7 +45,7 @@ fn main() {
         
         void main() {
             float brightness = dot(normalize(v_normal), normalize(u_light));
-            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 dark_color = vec3(0.5, 0.0, 0.0);
             vec3 regular_color = vec3(1.0, 0.0, 0.0);
             color = vec4(mix(dark_color, regular_color, brightness), 1.0);
         }
@@ -60,6 +60,7 @@ fn main() {
             write: true,
             ..Default::default()
         },
+        backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
         ..Default::default()
     };
 
@@ -92,7 +93,7 @@ fn main() {
                     winit::event::WindowEvent::RedrawRequested => {
                         let mut target = display.draw();
 
-                        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);   
+                        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
                         let matrix = [
                             [0.01, 0.0, 0.0, 0.0],
                             [0.0, 0.01, 0.0, 0.0],
